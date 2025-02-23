0665f3f24dacb9fc8ae5acc22068f20a418e525e
mjxjung <mjjung23@yonsei.ac.kr>
2025-02-23 20:26:00 +0900

    remove unnecessary backend files

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,11 @@
 ## fastapi 실행
 from fastapi import FastAPI
 from pydantic import BaseModel
-from ctgy_filter import filter_ctgy
-from details_filter import regenerate_query, filter_by_expanded_query
 from restaurant_filter import RestaurantFilter
 
 app = FastAPI()
 restaurant_filter = RestaurantFilter()
 
-# 아예 filter를 하나로 통합..
 class FilterRequest(BaseModel):
     ctgy: str  # 카테고리
     details: str  # 세부사항
@@ -30,29 +27,6 @@ async def filter_restaurants(request: FilterRequest):
     # 2차 필터링 (태그 매칭 수행)
     result = restaurant_filter.filter_expanded_query(open_restaurants, expanded_query)
 
-    return {"restaurants": result}
-
-class MenuRequest(BaseModel):
-    ctgy: str  # 카테고리명 또는 "아무거나"
-
-class DetailsRequest(BaseModel):
-    details: str
-
-@app.post("/filter_ctgy/")
-async def filter_restaurants_api(request: MenuRequest):
-    """
-    사용자가 입력한 메뉴 또는 카테고리 기반으로 식당 필터링 API
-    """
-    result = filter_ctgy(request.ctgy)
-    return {"restaurants": result}
-
-@app.post("/filter_details/")
-async def filter_details(request: DetailsRequest):
-    """
-    세부사항 기반 식당 필터링 API
-    """
-    expanded_query = regenerate_query(request.details)
-    result = filter_by_expanded_query(expanded_query)
     return {"restaurants": result}
 
 # FastAPI 실행
